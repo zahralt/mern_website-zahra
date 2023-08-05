af451f62c6ddc0566d59dec628614f4df9f59c76
Sari Nurharlita Azzahra <80371561+zahralt@users.noreply.github.com>
2023-08-05 14:13:39 +0700

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css'
+import Homepage from "./components/homepage/homepage"
+import Login from "./components/login/login"
+import Register from "./components/register/register"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from 'react';
+
+function App() {
+
+  const [ user, setLoginUser] = useState({})
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {
+              user && user._id ? <div className='homepage'><Homepage setLoginUser={setLoginUser} /></div> : <Login setLoginUser={setLoginUser}/>
+            }
+          </Route>
+          <Route path="/login">
+            <Login setLoginUser={setLoginUser}/> 
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
